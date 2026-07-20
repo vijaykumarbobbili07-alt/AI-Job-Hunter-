fe4e19133e4ce4a0e7672c968108f73f8e4f5c28
vijaykumarbobbili07-alt <vijaykumarbobbili07@gmail.com>
2026-07-20 14:52:27 +0530

    Create jobs.py

---
+++ jobs.py
@@ -0,0 +1,18 @@
+def get_jobs():
+    return [
+        {
+            "company": "Microsoft",
+            "role": "Power BI Developer",
+            "location": "Bangalore"
+        },
+        {
+            "company": "Accenture",
+            "role": "Data Analyst",
+            "location": "Hyderabad"
+        },
+        {
+            "company": "Deloitte",
+            "role": "BI Developer",
+            "location": "Chennai"
+        }
+    ]
